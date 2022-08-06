52323f917aea386a00d24eefaaa1a6b136d36779
Martin Schreier <m.stockhammer@web.de>
2022-08-06 23:34:14 +0200

    Adding help option

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <hidapi.h>
 #include <numeric> 
 #include <iostream>      
+#include <getopt.h>
 
 const unsigned short VENDOR_ID = 0x16c0;
 const unsigned short PRODUCT_ID = 0x0480;
@@ -43,7 +44,7 @@ int check_temp(hid_device *handle, int devices) {
                 sign[0] ='-';
             }
             
-            printf("%04d-%02d-%02dT%02d:%02d:%02d;%d;%d;%+.1f;%s;%.1f;°C;%d\n", loctm.tm_year+1900, loctm.tm_mon+1, loctm.tm_mday, loctm.tm_hour, loctm.tm_min, loctm.tm_sec, devices, device_id, ftemp, sign, ftemp, device_result_sum);
+            printf("%04d-%02d-%02dT%02d:%02d:%02d;%d;%d;%+.1f;%s;%.1f;°C\n", loctm.tm_year+1900, loctm.tm_mon+1, loctm.tm_mday, loctm.tm_hour, loctm.tm_min, loctm.tm_sec, devices, device_id, ftemp, sign, ftemp);
             device_result[device_id-1]=1;
         }
         device_result_sum = std::accumulate(device_result, device_result+devices, 0);
@@ -83,11 +84,14 @@ int print_adapter_info(hid_device *handle, int devices) {
     }
     printf("#   %-25s: %ls\n", "Product String", wstr);
 
+
     res = hid_get_serial_number_string(handle, wstr, MAX_STR);
     if (res == -1) {
         fprintf(stderr, "Could not get serial number string!\n");
         return 4;
     }
+    // printf doesn't like the chars from the serial number, 
+    // so we have to use cout for this output
     char prefix[32];
     sprintf(prefix, "#   %-25s: ", "Serial Number");
     std::cout << prefix << wstr << "\n";
@@ -99,23 +103,40 @@ int print_adapter_info(hid_device *handle, int devices) {
 int main(int argc, char **argv) {
     bool continuousMode = false;
     int c;
-    while ((c = getopt (argc, argv, "c")) != -1)
-    switch (c)
-      {
-      case 'c':
-        continuousMode = true;
-        break;
-      case '?':
-        if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return 1;
-      default:
-        abort ();
-      }
+    int option_index = 0;
+    static struct option long_options[] = {
+                   {"continuous",no_argument,0,'c'},
+                   {"help",no_argument,0,'h'},
+                   {0,0,0,0}
+    };
+    
+    while(1) {
+        c = getopt_long(argc, argv, "ch", long_options, &option_index);
+        // stopping loop, if all args are parsed
+        if (c == -1)
+            break;
+        switch (c)
+          {
+          case 'c':
+            continuousMode = true;
+            break;
+          case 'h':
+            printf("\ntempsense [-c|--continuous] [-h|--help]\n");
+            printf("  -c: Check values and print until the program is stopped. Otherwise only one sample is printed.\n");
+            printf("  -h: Print this usage\n\n");
+            return 0;
+          case '?':
+            if (isprint (optopt))
+              fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            else
+              fprintf (stderr,
+                       "Unknown option character `\\x%x'.\n",
+                       optopt);
+            return 1;
+          default:
+            abort ();
+          }
+    }
     
     hid_device *handle = hid_open(VENDOR_ID, PRODUCT_ID, nullptr);
     if (!handle) {
